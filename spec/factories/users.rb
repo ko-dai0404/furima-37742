e0ce97f88f52c6892a8cebae1b5f740bdc5eac4b
ko-dai0404 <rudo.kenkobastyle.19930404@gmail.com>
2022-04-18 20:57:51 +0900

    Userモデル単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 3)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6) + "1a"}
+    password_confirmation {password}
+    last_name             {'下村'}
+    first_name            {'晃大'}
+    last_name_kana        {'シモムラ'}
+    first_name_kana       {'コウダイ'}
+    birthday              {'19930404'}
+  end
+end
