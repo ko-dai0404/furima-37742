b6622140aec024e8a0ac6a424d350661ab3abc6f
ko-dai0404 <rudo.kenkobastyle.19930404@gmail.com>
2022-04-25 12:00:48 +0900

    Revert "Revert "購入機能テストコード実装""
    This reverts commit dc2ec1aabdf451f67a49224bb22c4e5bad21eaf6.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,35 @@
 class OrdersController < ApplicationController
+  binding.pry
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_root, except: [:index]
+
   def index
     @item = Item.find(params[:item_id])
     @order_purchase = OrderPurchase.new
+
   end
 
   def create
     @order_purchase = OrderPurchase.new(order_params)
     if @order_purchase.valid?
       @order_purchase.save
-       return redirect_to root_path
+      redirect_to root_path
     else
       @item = Item.find(params[:item_id])
-       render :index
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_purchase).permit(:postal, :prefectures_id, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+    params.require(:order_purchase).permit(:postal, :prefectures_id, :city, :address, :building, :telephone).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user.id == @item.user.id
+    binding.pry
   end
 end
